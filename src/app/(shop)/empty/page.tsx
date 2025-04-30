5817b6a78cd39579e237c400ee09a7ae5b987d6c
Fabrizio <fabrinucci99@gmail.com>
2025-04-29 21:41:42 -0300

    feat: Update empty cart page

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,7 +1,25 @@
+import { PrimaryLink } from '@/components/ui';
+import Image from 'next/image';
+
 export default function Empty() {
   return (
-    <div>
-      <h1>Empty</h1>
-    </div>
+    <main className='flex w-full flex-col items-center justify-center sm:flex-row'>
+      <div className='max-w-xs text-center'>
+        <h1 className='mb-6 text-3xl font-semibold antialiased sm:text-5xl'>
+          Tu carrito esta vacío
+        </h1>
+        <PrimaryLink href='/' aria-label='Volver al inicio'>
+          Volver al inicio
+        </PrimaryLink>
+      </div>
+      <div>
+        <Image
+          src='/imgs/carrito.webp'
+          alt='Carrito vacío'
+          height={350}
+          width={350}
+        />
+      </div>
+    </main>
   );
 }
