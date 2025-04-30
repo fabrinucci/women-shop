0c73478626dae7bb73109a35a506366478d300a2
Fabrizio <fabrinucci99@gmail.com>
2025-04-29 21:50:40 -0300

    feat: Add size selector

---
+++ src/components/product/size-selector/SizeSelector.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx';
+import { Size } from '@/interfaces/products';
+
+interface Props {
+  availableSizes: Size[];
+  selectedSize: Size;
+}
+
+export const SizeSelector = ({ selectedSize, availableSizes }: Props) => {
+  return (
+    <div>
+      <p className='mb-4 text-lg font-semibold md:mb-2'>Talles disponibles</p>
+      <div className='flex justify-center gap-2 md:justify-start'>
+        {availableSizes.map((size) => (
+          <button
+            className={clsx(
+              'bg-details hover:bg-secondary h-10 w-10 cursor-pointer rounded-lg p-2 transition-colors duration-300 ease-in hover:text-white',
+              { 'bg-secondary text-white': selectedSize === size },
+            )}
+            key={size}
+          >
+            {size}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
