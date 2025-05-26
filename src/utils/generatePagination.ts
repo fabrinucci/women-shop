98173c7235ecfe1594ef4a1f5c80618ad470f8c0
fabrinucci <fabrinuccidev@gmail.com>
2025-05-26 15:28:50 -0300

    feat: Add generatePagination

---
+++ src/utils/generatePagination.ts
@@ -0,0 +1,20 @@
+interface Props {
+  totalPages: number;
+  currentPage: number;
+}
+
+export const generatePagination = ({ totalPages, currentPage }: Props) => {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, i) => i + 1);
+  }
+
+  if (currentPage <= 3) {
+    return [1, 2, 3, 4, '...', totalPages - 1, totalPages];
+  }
+
+  if (currentPage >= totalPages - 2) {
+    return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages];
+  }
+
+  return [1, 2, '...', currentPage, '...', totalPages - 1, totalPages];
+};
