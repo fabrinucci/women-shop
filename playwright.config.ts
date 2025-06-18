63a2163bca17526714bcaae1b81a682a1402b2f1
fabrinucci <fabrinuccidev@gmail.com>
2025-06-18 20:45:36 -0300

    ci: Fix playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
 
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
